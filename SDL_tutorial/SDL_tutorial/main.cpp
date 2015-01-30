e8787ea4cb141d8227e5caa312b1a047d3ad6cd0
David Morton <davidmortonmail@gmail.com>
2015-01-30 10:56:14 +0000

    added SDL2.dll to project folder to fix issue of not being able to find it; also added window and filled white

--- SDL_tutorial/SDL_tutorial/main.cpp
+++ SDL_tutorial/SDL_tutorial/main.cpp
@@ -26,9 +26,27 @@ int main(int argc, char* args[])
 		{
 			printf("window could not be created! SDL error %s\n", SDL_GetError());
 		}
+		else
+		{
+			// get window surface
+			screenSurface = SDL_GetWindowSurface(window);
+
+			// fill the surface white
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+			// update the surface
+			SDL_UpdateWindowSurface(window);
+
+			// wait two seconds
+			SDL_Delay(2000);
+		}
 	}
+
+	// destroy window
+	SDL_DestroyWindow(window);
+
 	//Quit SDL
-	SDL_Quit();
+	SDL_Quit(); 
 
 	return 0;
 }
