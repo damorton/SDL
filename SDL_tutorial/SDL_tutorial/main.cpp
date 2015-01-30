bb3cafb974e668f8992df3c7e393626ba25b90f6
David Morton <davidmortonmail@gmail.com>
2015-01-30 10:40:44 +0000

    added create window and initialized it

--- SDL_tutorial/SDL_tutorial/main.cpp
+++ SDL_tutorial/SDL_tutorial/main.cpp
@@ -14,8 +14,19 @@ int main(int argc, char* args[])
 	SDL_Surface* screenSurface = NULL;
 
 	//Start SDL
-	SDL_Init(SDL_INIT_EVERYTHING);
-
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		// create window
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOWEVENT_SHOWN);
+		if (window == NULL)
+		{
+			printf("window could not be created! SDL error %s\n", SDL_GetError());
+		}
+	}
 	//Quit SDL
 	SDL_Quit();
 
