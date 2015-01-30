c985dcd582ef9fc514eef4a3e0a0e99677543063
David Morton <davidmortonmail@gmail.com>
2015-01-30 10:33:59 +0000

    tutorial 1 hello sdl setup on laptop

--- SDL_tutorial/SDL_tutorial/main.cpp
+++ SDL_tutorial/SDL_tutorial/main.cpp
@@ -1,7 +1,18 @@
-#include "SDL.h"
+#include <SDL.h>
+#include <stdio.h>
+
+// screen dimensions
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[])
 {
+	// window
+	SDL_Window* window = NULL;
+
+	// surface
+	SDL_Surface* screenSurface = NULL;
+
 	//Start SDL
 	SDL_Init(SDL_INIT_EVERYTHING);
 
